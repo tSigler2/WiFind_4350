ccb41925f2d970b7c2e3ea7421a30c2907e1e891
Kmorale12 <Kevingustavo.kmf@gmail.com>
2024-03-07 08:23:05 -0500

    Added Header and tried adding links

--- wifi-finder/src/App.jsx
+++ wifi-finder/src/App.jsx
@@ -1,14 +1,18 @@
 // App.js
 import React from 'react';
 import Header from './components/Header.jsx';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      
+     <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact  /> // Replace 'Home' with your desired component
+        </Routes>
+      </Router>
     </div>
   );
 }
--- wifi-finder/src/components/Header.jsx
+++ wifi-finder/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import '../App.css';
-
+import { Link } from 'react-router-dom';
+import logo from '../assets/download.png';
 function Header() {
     return (
         <header className="header">
-            <nav className="navbar">
-                <h1 className="navbar__title">WiFi Finder & Rentals</h1>
+            <nav className="navbar-container">
+                <div className="navbar__logo">
+                    <Link to="/home">
+                        <img src={logo} alt="Logo" className='logo'/>
+                        <h1>Wifi-Finder and Renter</h1>
+                    </Link>
+                </div>
                 <div className="navbar__links">
-                    <a href="/home">Home</a>
                     <a href="/rent">Rent WiFi</a>
-                    <a href="/sell">Sell WiFi</a>
-                    <a href="/about">About</a>
+                    <a href='/rentyourwifi'>Rent Your Wifi</a>
                     <a href="/contact">Contact</a>
                     <a href="/login">Login</a>
                 </div>
