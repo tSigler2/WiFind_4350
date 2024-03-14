6696cc144a81604e5b15ed703620f7ec21a65f6e
Kmorale12 <Kevingustavo.kmf@gmail.com>
2024-03-13 22:01:03 -0400

    Added wifi options

--- wifi-finder/src/pages/Rent.jsx
+++ wifi-finder/src/pages/Rent.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "../App.css";
+import data from "../rentOptions.json";
 
 function Rent() {
+    const rentOptions = data.options; // Access the "options" property of the imported data
+
     return (
         <div>
-        <h1>Rent</h1>
-        <p>Rent WiFi</p>
+            <h1>Rent</h1>
+            {rentOptions.map((option) => (
+                <div key={option.name} className="card">
+                    <h2>{option.name}</h2>
+                    <p>{option.description}</p>
+                    <p>Price: {option.price}</p>
+                    <ul>
+                        {option.features.map((feature) => (
+                            <li key={feature}>{feature}</li>
+                        ))}
+                    </ul>
+                    <button onClick={() => addToCart(option)}>Add to Cart</button>
+                </div>
+            ))}
         </div>
     );
-    }
-export default Rent;    
+}
+
+export default Rent;
