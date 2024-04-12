3b63a140e55989cdbfd9fa4ad2bc5da30f43cadc
Kmorale12 <Kevingustavo.kmf@gmail.com>
2024-04-11 21:52:38 -0400

    Shop-now button

--- src/wiFind.client/src/components/HeroSection.jsx
+++ src/wiFind.client/src/components/HeroSection.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import the Link component
 import backgroundImage from '../images/wifi1.jpg'; // Import the image
 
 function HeroSection() {
   return (
     <div className="hero-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <h1>Connect Anywhere, Anytime</h1>
-      <button className="hero-button">Shop Now</button> 
+      <Link to="/list" className="hero-button">Shop Now</Link> {/* Use the Link component to create a link to the listing page */}
     </div>
   );
 }
